4eaee1372cdb26d4cb15bcda90510e68ae1ff6aa
Md Saquib Hussain <mdsaquib28official@gmail.com>
2026-04-01 16:37:16 +0530

    Refactor print examples and add logical operators demonstration

--- 01basics/01basic-syntax.py
+++ 01basics/01basic-syntax.py
@@ -11,16 +11,6 @@
 
 
 
-# Print command
-print("This is python print")
-
-print("First Print","Second Print")
-
-print("First","Second","Third",sep=",",end="") #Changed behaviour using the optional parameter
-print("Fourth")
-
-print("this is first line\nthis is next line")
-
 
 
 # This is a single line comment
---
+++ 01basics/01basic-syntax/05print.py
@@ -0,0 +1,10 @@
+
+# Print command
+print("This is python print")
+
+print("First Print","Second Print")
+
+print("First","Second","Third",sep=",",end="") #Changed behaviour using the optional parameter
+print("Fourth")
+
+print("this is first line\nthis is next line")
